16392d60c23380906a5b8efb3cbf65d7245ded65
Davi Negreiros <41302833+davinegreiiros@users.noreply.github.com>
2021-04-05 10:45:49 -0300

    Componente Card #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Card from './components/layout/Card';
 import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from "./components/basicos/Primeiro";
 import ComParematro from "./components/basicos/ComParametro";
@@ -9,14 +10,27 @@ export default function App(props) {
   return (
     <div id="app">
       <h1>Fundamentos React</h1>
-      <Aleatorio min={1} max={60}/>
-      <Fragmento />
+
+      <Card titulo="#04 - Desafio Aleatorio">
+        <Aleatorio min={1} max={60} />
+      </Card>
+
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
+
+      <Card titulo="#02 - Com Parametro">
       <ComParematro
         titulo="Situação do aluno"
         aluno="Davi Negreiros"
         nota={9.5}
       />
-      <Primeiro />
+      </Card>
+
+      <Card titulo="#01 -  Primeiro Componente">
+        <Primeiro />
+      </Card>
+  
     </div>
   );
 }
