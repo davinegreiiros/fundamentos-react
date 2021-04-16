60dd9ab137c9f29540f6c58ff620684cee30b472
Davi Negreiros <41302833+davinegreiiros@users.noreply.github.com>
2021-04-16 09:43:09 -0300

    Componente Card #03

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,15 @@ export default function App(props) {
       <h1>Fundamentos React</h1>
 
       <div className="Cards">
-      <Card titulo="#04 - Desafio Aleatorio">
+      <Card titulo="#04 - Desafio Aleatorio" color="#FA6900">
         <Aleatorio min={1} max={60} />
       </Card>
 
-      <Card titulo="#03 - Fragmento">
+      <Card titulo="#03 - Fragmento" color="#E94C6F">
         <Fragmento />
       </Card>
 
-      <Card titulo="#02 - Com Parametro">
+      <Card titulo="#02 - Com Parametro" color="#E8B71A">
       <ComParematro
         titulo="Situação do aluno"
         aluno="Davi Negreiros"
@@ -29,7 +29,7 @@ export default function App(props) {
       />
       </Card>
 
-      <Card titulo="#01 -  Primeiro Componente">
+      <Card titulo="#01 -  Primeiro Componente" color="#588C73">
         <Primeiro />
       </Card>
   
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -6,8 +6,14 @@ import React from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => {
+
+    const cardStyle = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00',
+    }
+
     return (
-    <div className="Card">
+    <div className="Card" style={cardStyle}>
      <div className="Title">{props.titulo}</div>
       <div className="Content">
         {props.children}
